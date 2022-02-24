f04106765504056d3839696965244022b8f07803
hungran <26101787+hungran@users.noreply.github.com>
2022-02-24 18:17:23 +0700

    Create sh

---
+++ mfa-cli.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh 
+# . ./mfa-cli.sh to run on current working shell
+# specify your MFA_DEVICE_ARN
+
+if [ -z ${MFA_DEVICE_ARN}]; then
+    echo "Please specify the MFA_DEVICE_ARN"
+    exit 1
+fi
+vared -p 'Please enter MFA ARN: ' -c MFA_DEVICE_ARN
+echo "You entered '$MFA_CODE'"
+vared -p 'Please enter MFA code: ' -c MFA_CODE
+echo "You entered '$MFA_CODE'"
+
+if [ -z ${MFA_DEVICE_ARN}]; then
+    echo "Please specify the MFA_DEVICE_ARN"
+    exit 1
+fi
+if [ -z ${MFA_CODE}]; then
+    echo "Please specify the MFA_DEVICE_ARN"
+    exit 1
+fi
+
+COMMAND=`/usr/local/bin/aws --profile VTIA --output text sts get-session-token \
+    --serial-number $MFA_DEVICE_ARN \
+    --token-code $MFA_CODE`
+
+echo $COMMAND
+CREDS=$COMMAND
+
+KEY=$(echo "${CREDS}" | cut -f2)
+SECRET=$(echo $CREDS | cut -f4)
+SESS_TOKEN=$(echo $CREDS | cut -f5)
+
+echo "export AWS_ACCESS_KEY_ID=$KEY"
+echo "export AWS_SECRET_ACCESS_KEY=$SECRET"
+echo "export AWS_SESSION_TOKEN=$SESS_TOKEN"
+
+export AWS_ACCESS_KEY_ID=$KEY
+export AWS_SECRET_ACCESS_KEY=$SECRET
+export AWS_SESSION_TOKEN=$SESS_TOKEN
+
+# check if script has been sourced or executed
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+if [ $sourced -eq 1 ]; then
+    echo "Script was sourced."
+else
+    echo "Script was executed, starting subshell."
+    zsh -l
+fi
